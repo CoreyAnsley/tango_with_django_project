f820e532883cd0149994d14aa8f34508798e04cf
Corey Ansley <2266643A@student.gla.ac.uk>
2018-02-09 18:08:13 +0000

    chpt 5

--- rango/admin.py
+++ rango/admin.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from rango.models import Category, Page
+
+# Add in this class to customise the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
 
 # Register your models here.
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page)
+
+
+
